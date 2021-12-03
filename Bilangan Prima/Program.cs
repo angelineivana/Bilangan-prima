8f7c2a9dc62982ec9aaa3129272c585f2422b9cf
angelineivana <angelineivana01@student.ciputra.ac.id>
2021-12-03 10:20:18 +0700

    Add project files.

---
+++ Bilangan Prima/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bilangan_Prima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan bilangan : ");
+            int bil = Convert.ToInt16(Console.ReadLine());
+            int jumlah = 0;
+
+            for (int i = 1; i <= bil; i++)
+            {
+                if (bil % i == 0)
+                {
+                    jumlah++;
+                }
+            }
+            if (jumlah == 2)
+            {
+                Console.WriteLine($"Bilangan {bil} adalah bilangan prima");
+            }
+            else
+            {
+                Console.WriteLine($"Bilangan {bil} bukan bilangan prima");
+            }
+        }
+    }
+}
